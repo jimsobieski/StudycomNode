81572c1f1311ad24bfdb3531ef1a948c33fb0175
Sébastien Taton <jimsobieski21@gmail.com>
2017-10-03 14:18:33 +0200

    create database connexion and socket server

--- app.js
+++ app.js
@@ -43,4 +43,64 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// creation connexion bdd
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database : 'studycom'
+});
+
+
+db.connect(function(err) {
+  if(err) {
+      console.log(err);
+  }
+  else {
+      console.log("Connected!");
+      db.query("SELECT login FROM users", function (err, results, fields) {
+        console.log(results);
+      });
+  }
+});
+
+
+//SOCKETS
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function (req, res) {
+    fs.readFile('./index.html', 'utf-8', function (error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('joinChannel', function (channel) {
+        console.log('Un user est connecté aut topic !');
+        socket.join(channel);
+        io.in(channel).emit('joinChannel', 'hello topic');
+
+        socket.on('newMessage', function (message) {
+            console.log(message);
+            io.in(channel).emit('newMessage', message);
+        });
+
+    });
+});
+server.listen(8080);
+
 module.exports = app;
